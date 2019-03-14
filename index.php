0d85ac24a8aef3a647748195f2f1688f17951579
juacosoft <joaquinalfonso.martinez@gmail.com>
2019-03-14 13:41:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,17 @@
-<?php 
+<?php
+header("Content-Type: application/json");
+ob_start();
+$requestBody = file_get_contents('php://input'); 
+$json = json_decode($requestBody, true);
 
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if(isset($update["queryResult"]["action"])) {
-    processMessage($update);
-}
+$text = $json['result']['resolvedQuery'];
+$response = json_encode(array(
+        "source" => "webhook",
+        "speech" => $text,
+        "displayText" => $text,
+        "contextOut" => array()
+    ));
 
-
-function processMessage($update) {
-    //el action getcontent que hemos indicado en el intent (action and parameters)
-    if($update["queryResult"]["action"] == "getcontent"){
-        //parametros creados en el intent. En nuestro caso vendrá el nombre de la ciudad
-        $params = $update["queryResult"]["parameters"];
-        //obtenemos el nombre de la ciudad
-        $nombreCliente = $params["phone"];
-        
-        //creamos el mensaje a mostrar al usuario
-        sendMessage(array(
-            "fulfillmentText" => "Gracias sr(a)".$nombreCliente." Un asesor se comunicara con usted",
-            "source"=> "Juaco.info"
-        ));
-    }else{
-        //mensaje de error
-        sendMessage(array(
-            "fulfillmentText"=> "Error no se recibieron datos",
-            "source"=> "juaco.com"
-        ));
-    }
-}
-
-
+ob_end_clean();
+echo $response;
 ?>
